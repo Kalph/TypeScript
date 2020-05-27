24adf163f5370c7c02a0609ddc053ee28bef704d
Kalph <kjskhj04366@naver.com>
2020-05-27 19:15:45 +0900

    crawling test3

--- puppeteer-sample/src/asy_craw.ts
+++ puppeteer-sample/src/asy_craw.ts
@@ -5,23 +5,26 @@ async function getFile () {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('https://www.ytn.co.kr/_cs/_ln_0103_202005271609395005_005.html')
-  const name = await page.$eval('#zone1 > div.article_tit', node => (node as HTMLElement).innerText);
-  // const result = await page.evaluate(() => {
-  //   const name = (document.querySelector('#zone1 > div.article_tit')as HTMLElement).innerText
-  //   return {name}
-  // })
+  // const name = await page.$eval('#zone1 > div.article_tit', node => (node as HTMLElement).innerText);
+  
+  // const name = await page.evaluate(() => {
+  //     return (document.querySelector('#zone1 > div.article_tit') as HTMLElement).innerText
+  //  })
+
+  const result = await page.evaluate(() => {
+    const name = (document.querySelector('#zone1 > div.article_tit')as HTMLElement).innerText
+    const time = (document.querySelector('#newsContent > div.extra_info')as HTMLElement).innerText
+    const content = (document.querySelector('#CmAdContent > span')as HTMLElement).innerText
+
+    return {name, time, content}
+  })
 
   // evaluate로 가져온 결과는 이렇게 변수에 저장되는걸 볼 수 있음.
-  console.log(name);
+  console.log(result);
 
-  fs.writeFile('outputs/async_crawling.json', JSON.stringify(name), function(err:any) {
+  fs.writeFile('outputs/async_crawling.json', JSON.stringify(result), function(err:any) {
     if(err) console.log('error', err);
   });
-
-  // fs.writeFile('outputs/async_crawling.json', JSON.stringify(result), function(err:any) {
-  //   if(err) console.log('error', err);
-  // });
-
   // 마지막에는 launch한 브라우저를 닫아야함.
   await browser.close()
 }
